61098ec0b2cbb18b165a7044f4612b6ba0f8dac9
Burapa2025 <guy6176@gmail.com>
2026-08-27 18:12:24 +0700

    submitted

---
+++ docs/labs/week-05/vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/engse203-student-labs-68543210032-7/labs/week-05/',
+});
